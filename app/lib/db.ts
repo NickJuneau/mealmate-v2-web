1ecc44974a2a7a75513d6bef37b2b477c7c9f114
NickJuneau <njuneau@email.sc.edu>
2026-02-09 16:13:27 -0500

    added routing for google auth

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,17 +1,47 @@
-// lib/db.ts
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
+// 1. Create a database connection pool
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+// 2. Initialize the Prisma Driver Adapter
+const adapter = new PrismaPg(pool);
+
 export const prisma =
   global.prisma ??
+  // 3. Pass the adapter to the PrismaClient constructor
   new PrismaClient({
-    log: ['query'], // optional; remove if noisy
+    adapter,
+    log: ['query'],
   });
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma;
 }
+
+
+
+// // lib/db.ts
+// import { PrismaClient } from '@prisma/client';
+
+
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var prisma: PrismaClient | undefined;
+// }
+
+// export const prisma =
+//   global.prisma ??
+//   new PrismaClient({
+//     log: ['query'], // optional; remove if noisy
+//   });
+
+// if (process.env.NODE_ENV !== 'production') {
+//   global.prisma = prisma;
+// }
